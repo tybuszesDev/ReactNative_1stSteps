028a47ff2db787f1e27061f03d00d4b88f4c38ca
Łukasz Tybuszewski <tybuszes@protonmail.com>
2020-05-19 23:38:32 +0200

    Using State #4

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("Lukasz");
+  const [person, setPerson] = useState({ name: "Tomek", age: 25 });
+
+  const handleButton = () => {
+    setName("Roman");
+    setPerson({ name: "filip", age: 45 });
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello, World!</Text>
-      </View>
-      <View style={styles.body}>
-        <Text style={styles.boldText}>
-          Lorem ipsum <Text>test</Text> dolor sit amet consectetur
-        </Text>
-        <Text>Lorem ipsum dolor sit amet consectetur</Text>
-        <Text>Lorem ipsum dolor sit amet consectetur</Text>
+      <Text>My name is {name}</Text>
+      <Text>
+        His name is {person.name} and his age is {person.age}
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Button title="update state" onPress={handleButton} />
       </View>
     </View>
   );
@@ -25,16 +31,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  header: {
-    backgroundColor: "pink",
-    padding: 20,
-  },
-  boldText: {
-    fontWeight: "bold",
-  },
-  body: {
-    backgroundColor: "yellow",
-    padding: 20,
-    fontWeight: "bold",
+  buttonContainer: {
+    marginTop: 20,
   },
 });
