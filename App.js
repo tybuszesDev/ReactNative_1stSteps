9768d6b613dd773de3a0d3c6164f88df7ca3074e
Łukasz Tybuszewski <tybuszes@protonmail.com>
2020-05-19 23:11:27 +0200

    Views, Text and Styles #3

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello, World!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>
+          Lorem ipsum <Text>test</Text> dolor sit amet consectetur
+        </Text>
+        <Text>Lorem ipsum dolor sit amet consectetur</Text>
+        <Text>Lorem ipsum dolor sit amet consectetur</Text>
+      </View>
     </View>
   );
 }
@@ -12,8 +21,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "pink",
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: "bold",
+  },
+  body: {
+    backgroundColor: "yellow",
+    padding: 20,
+    fontWeight: "bold",
   },
 });
